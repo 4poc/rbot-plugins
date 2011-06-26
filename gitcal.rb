dcf8f52730fa908d224e75f041fa4f4cefdf2038
Matthias -apoc- Hecker <apoc@sixserv.org>
2011-06-26 21:40:16 +0200

    thats better;)

--- gitcal.rb
+++ gitcal.rb
@@ -9,10 +9,10 @@ class GithubCalPlugin < Plugin
     page = agent.get url
 
     num = page.search '.longest_streak a .num'
-    streak = num.first.content.split("\n").map { |s| s.strip!; s+=' ' }
+    streak = num.first.content.split("\n").map({ |s| s.strip! }).join ' '
 
     num = page.search '.current_streak .num'
-    streak_current = num.first.content.split("\n").map { |s| s.strip!; s+=' ' }
+    streak_current = num.first.content.split("\n").map({ |s| s.strip! }).join ' '
 
     m.reply "Calendar about nothing.. current/longest streak:#{streak_current}/#{streak}(#{url})"
   end
